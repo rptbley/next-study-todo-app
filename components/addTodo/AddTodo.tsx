1f0f0f036591c3ca9e96f5810eaefe0c9f121303
방우현 <docter4@naver.com>
2021-12-30 14:37:39 +0900

    추가하기

--- components/addTodo/AddTodo.tsx
+++ components/addTodo/AddTodo.tsx
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import palette from "../../styles/palette";
+import AddBtnIcon from "../../public/add_btn.svg";
+import { useState } from "react";
+import { TodoType } from "../../types/todo";
+import { addTodoAPI } from "../../lib/api/todos";
+import { useRouter } from "next/router";
+
 
 const Container = styled.div`
     padding: 16px;
@@ -21,9 +28,80 @@ const Container = styled.div`
             font-size: 14px;
         }
     }
+
+    .add-todo-colors-wrapper {
+        width: 100%;
+        margin-top: 16px;
+        display: flex;
+        justify-content: space-between;
+
+        .add-todo-color-list {
+            display: flex;
+            button {
+                width: 24px;
+                height: 24px;
+                margin-right: 16px;
+                border: 0;
+                outline: 0;
+                border-radius: 50%;
+                &:last-child {
+                    margin: 0;
+                }
+            }
+            .add-todo-selected-color {
+                border: 2px solid black !important;
+            }
+        }
+    }
+    .bg-blue {
+        background-color: ${palette.blue}
+    }
+    .bg-green {
+        background-color: ${palette.green}
+    }
+    .bg-navy {
+        background-color: ${palette.navy}
+    }
+    .bg-orange {
+        background-color: ${palette.orange}
+    }
+    .bg-red {
+        background-color: ${palette.red}
+    }
+    .bg-yellow {
+        background-color: ${palette.yellow}
+    }
+
+    textarea {
+        width: 100%;
+        border-radius: 5px;
+        height: 300px;
+        border-color: ${palette.gray};
+        margin-top: 12px;
+        resize: none;
+        outline: none;
+        padding: 12px;
+        font-size: 16px;
+    }
 `;
 
 const AddTodo: React.FC = () => {
+    const router = useRouter();
+    const [text, setText] = useState("");
+    const [selectedColor, setSelectedColor] = useState<TodoType["color"]>();
+
+    const addTodo = async () => {
+        try {
+            if(!text || !selectedColor) {
+                return alert("색상과 할 일을 입력해주세요.")
+            }
+            await addTodoAPI({ text, color: selectedColor });
+            console.log('추가했습니다.');
+            router.push({pathname: "/"})
+        } catch(err) {
+            console.log(err);
+        }
+    }
     return (
         <Container>
             <div className="add-todo-header">
@@ -31,11 +109,29 @@ const AddTodo: React.FC = () => {
                 <button
                     type="button"
                     className="add-todo-submit-button"
-                    onClick={() => {}}
+                    onClick={addTodo}
                 >
                     추가하기
                 </button>
             </div>
+            <div className="add-todo-colors-wrapper">
+                <div className="add-todo-color-list">
+                    {["red", "orange", "yellow", "green", "blue", "navy"].map((color, index) => (
+                        <button
+                            key={index}
+                            type="button"
+                            className={`bg-${color} add-todo-color-button ${color === selectedColor ? "add-todo-selected-color" : ""}`}
+                            onClick={() => {setSelectedColor(color as TodoType["color"])}}
+                        />
+                    ))}
+                </div>
+                <AddBtnIcon />
+            </div>
+            <textarea 
+                value={text}
+                onChange={(e) => setText(e.currentTarget.value)}
+                placeholder="할 일을 입력해주세요."
+            />
         </Container>
     )
 }
--- lib/api/todos.ts
+++ lib/api/todos.ts
@@ -1,6 +1,15 @@
 import axios from './index';
 import { TodoType } from '../../types/todo';
 
-export const getTodosAPI = () => axios.get<TodoType[]>("api/todos");
+interface AddTodoAPIBody {
+    text: string;
+    color: TodoType["color"];
+}
 
-export const checkTodoAPI = (id: number) => axios.patch(`api/todos/${id}`);
+export const getTodosAPI = () => axios.get<TodoType[]>("/api/todos");
+
+export const checkTodoAPI = (id: number) => axios.patch(`/api/todos/${id}`);
+
+export const addTodoAPI = (body: AddTodoAPIBody) => {
+    axios.post("/api/todos", body);
+}
